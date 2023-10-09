8986274cc506f4361a8f325254548eadac5afdce
Sonya Shakirova <smshakirova@mail.ru>
2023-10-09 13:18:57 +0300

    test: add filter tests

--- spec/integration/Filter.spec.tsx
+++ spec/integration/Filter.spec.tsx
@@ -1,9 +1,62 @@
-describe('Список задач', () => {
-    // не содержит выполненные задачи
-    // после нажатия на кнопку фильтрации
-    it.todo('с включенным фильтром');
-    
-    // показывает как выполненные, так и не выполненные задачи
-    // после повторного нажатия на кнопку фильтрации
-    it.todo('с выключенным фильтром');
+import { render, screen } from "@testing-library/react";
+import { TaskList } from "src/modules/TaskList";
+import { JestStoreProvider } from "../utils/JestStoreProvider";
+import ue from "@testing-library/user-event";
+import * as taskSliceModule from "src/store/taskSlice";
+
+const userEvent = ue.setup();
+
+const items = [
+    {
+        id: "1",
+        header: "купить молоко",
+        done: true,
+    },
+    {
+        id: "2",
+        header: "купить хлеб",
+        done: false,
+    },
+];
+
+describe("Список задач", () => {
+    it("первоначальное состояние: содержит все задачи", () => {
+        jest.spyOn(taskSliceModule, "tasksSelector").mockReturnValue(items);
+
+        render(<TaskList />, { wrapper: JestStoreProvider });
+
+        const listItems = screen.getAllByRole("listitem");
+
+        expect(listItems).toHaveLength(2);
+    });
+
+    it("с включенным фильтром: не содержит выполненные задачи", async () => {
+        jest.spyOn(taskSliceModule, "tasksSelector").mockReturnValue(items);
+
+        render(<TaskList />, { wrapper: JestStoreProvider });
+
+        const filterBtn = screen.getByRole("button", { name: "фильтровать" });
+
+        await userEvent.click(filterBtn);
+
+        const checkboxElements = screen.getAllByRole("checkbox");
+
+        checkboxElements.forEach((el) => {
+            expect(el).not.toBeChecked();
+        });
+    });
+
+    it("с выключенным фильтром: сожержит все задачи", async () => {
+        jest.spyOn(taskSliceModule, "tasksSelector").mockReturnValue(items);
+
+        render(<TaskList />, { wrapper: JestStoreProvider });
+
+        const filterBtn = screen.getByRole("button", { name: "фильтровать" });
+
+        await userEvent.dblClick(filterBtn);
+
+        const listItems = screen.getAllByRole("listitem");
+
+        expect(listItems).toHaveLength(2);
+    });
 });
