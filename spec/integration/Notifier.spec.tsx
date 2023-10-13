1e917e5975fe07012a28804cfab965bc4b80a133
Sonya Shakirova <smshakirova@mail.ru>
2023-10-13 21:35:37 +0300

    test: add notifier tests

--- spec/integration/Notifier.spec.tsx
+++ spec/integration/Notifier.spec.tsx
@@ -1,7 +1,49 @@
 import { render, screen } from "@testing-library/react";
-import { Notifier } from "src/components/Notifier";
+import ue from "@testing-library/user-event";
+import { App } from "src/App";
 
-describe('Оповещение при вополнении задачи', () => {
-    it.todo('появляется и содержит заголовок задачи');
-    it.todo('одновременно может отображаться только одно');
+describe("Оповещение при вополнении задачи", () => {
+  const userEvent = ue.setup({
+    advanceTimers: jest.advanceTimersByTime,
+  });
+
+  it("появляется и содержит заголовок задачи", async () => {
+    render(<App />);
+
+    const inputEl = screen.getByRole("textbox");
+    const addBtnEl = screen.getByRole("button", { name: /Добавить/i });
+
+    await userEvent.type(inputEl, "купить печеньки");
+    await userEvent.click(addBtnEl);
+
+    const taskEl = screen.getByText("купить печеньки");
+
+    await userEvent.click(taskEl);
+
+    const notifierEl = screen.getByTestId("notifier");
+
+    expect(notifierEl.innerHTML).toContain("купить печеньки");
+  });
+
+  it("одновременно может отображаться только одно", async () => {
+    render(<App />);
+
+    const inputEl = screen.getByRole("textbox");
+    const addBtnEl = screen.getByRole("button", { name: /Добавить/i });
+
+    await userEvent.type(inputEl, "первая задача");
+    await userEvent.click(addBtnEl);
+    await userEvent.type(inputEl, "вторая задача");
+    await userEvent.click(addBtnEl);
+
+    const task1 = screen.getByText("первая задача");
+    const task2 = screen.getByText("вторая задача");
+
+    await userEvent.click(task1);
+    await userEvent.click(task2);
+
+    const notifiers = screen.getAllByTestId("notifier");
+
+    expect(notifiers).toHaveLength(1);
+  });
 });
